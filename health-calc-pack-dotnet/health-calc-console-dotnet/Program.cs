0fa2b3cd149f87884fd5deb56bc09776ca71c7a4
Jonathan <s.mieldazis@hotmail.com>
2022-11-24 02:40:50 -0300

    adjust console app

--- health-calc-pack-dotnet/health-calc-console-dotnet/Program.cs
+++ health-calc-pack-dotnet/health-calc-console-dotnet/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using health_calc_pack_dotnet;
 
-Console.WriteLine("Hello, World!");
+Console.Write("Altura: ");
+var height = Console.ReadLine();
+
+Console.Write("Peso: ");
+var weight = Console.ReadLine();
+
+Console.Write("Sexo: ");
+var sexo = Console.ReadLine();
+
+IMC imc = new IMC();
+double result = imc.Calc(double.Parse(height), double.Parse(weight));
+string category = imc.GetIMCCategory(result);
+
+Console.WriteLine("IMC: " + category);
+
+var SexoEnum = (sexo == "F") ? health_calc_pack_dotnet.Enums.SexoEnum.Feminino : health_calc_pack_dotnet.Enums.SexoEnum.Masculino;
+
+Macronutriente macro = new Macronutriente();
+var ResultMacronutrientes = macro.Calc(SexoEnum,
+    double.Parse(height),
+    double.Parse(weight),
+    health_calc_pack_dotnet.Enums.NivelAtividadeFisicaEnum.BastanteAtivo,
+    health_calc_pack_dotnet.Enums.ObjetivoFisicoEnum.Bulking);
+
+Console.WriteLine($"O seu consumo de macronutrientes deve ser => " +
+                  $"Carboidratos: {ResultMacronutrientes.Carboidratos} gramas, " +
+                  $"Proteinas: {ResultMacronutrientes.Proteinas} gramas, " +
+                  $"Gorduras: {ResultMacronutrientes.Gorduras} gramas");
+
+Console.ReadKey();
